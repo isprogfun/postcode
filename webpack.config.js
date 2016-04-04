c1d0c3ffb64f009abdad0a4b725adf46e0481633
Ivan Shcherbakov <isprogfun@gmail.com>
2016-04-04 20:50:09 +0300

    More dist changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, 'app/scripts/main.jsx'),
     output: {
-        path: path.resolve(__dirname, 'app'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.min.js'
     },
     devtool: 'source-map',
